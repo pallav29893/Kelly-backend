65330595bf0a47d3d37ab829773ba55363fd1229
pallav29893 <pallav869@gmail.com>
2024-09-06 19:11:28 +0530

    update templates

--- kelly/views.py
+++ kelly/views.py
@@ -7,3 +7,9 @@ def home(request):
 
 def blog(request):
     return render(request, 'blog.html')
+
+def about(request):
+    return render(request,'about us.html')
+
+def contact(request):
+    return render(request, 'contact us.html')
